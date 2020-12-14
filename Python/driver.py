c8f1016185d96ff524ae58b9bf10aa94b3fdd2eb
Sergio Jimenez <sdjimenez@telefonica.net>
2020-12-15 00:26:31 +0100

    Reto clase 29

---
+++ Python/driver.py
@@ -0,0 +1,5 @@
+from account import Account
+
+class Driver(Account):
+    def __init__(self,name,document):
+        super(Driver,self).__init__(name,document)
---
+++ Python/user.py
@@ -0,0 +1,5 @@
+from account import Account
+
+class User(Account):
+    def __init__(self,name,document):
+        super(User,self).__init__(name,document)
