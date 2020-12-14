0e1e37e48d2ca529dbf8fa429da9acfeb56b771b
Sergio Jimenez <sdjimenez@telefonica.net>
2020-12-14 04:05:00 +0100

    Practica herencia java y php video 25

---
+++ Java/Uberx.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class Uberx extends Car {
+    
+    String brand;
+    String model;
+
+    public Uberx(String license,Account driver,String brand, String model){
+        super(license, driver);
+        this.brand = brand;
+        this.model = model;
+    }
+}
---
+++ PHP/UberPool.php
@@ -0,0 +1,18 @@
+<?php
+
+include "Car.php";
+
+class UberPool extends Car
+{
+
+    public $brand;
+    public $model;
+    
+    public function __construct($licence,$driver,$brand,$model){
+
+        parent::__construct($licence,$driver);
+
+        $this->brand = $brand;
+        $this->model = $model;
+    }
+}
