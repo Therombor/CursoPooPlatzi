8213453b94c8ee9bfb80b526db16fb1eda759bba
Sergio Jimenez <sdjimenez@telefonica.net>
2020-12-14 23:51:12 +0100

    Ejercicio clase 27

---
+++ JS/UberBlack.js
@@ -0,0 +1,7 @@
+class UberBlack extends Car{
+    constructor(license,driver,typeCarAccepted,seatsMaterials){
+        super(license,driver);
+        this.typeCarAccepted = typeCarAccepted;
+        this.seatsMaterials = seatsMaterials;
+    }
+}
--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,11 @@
-var car = new Car('32424ASDFA',new Account('Andres','64565AS'))
+var car = new Car("32424ASDFA",new Account("Andres","64565AS"))
 
 car.passenger = 4;
 
 car.printDataCar();
+
+var car2 = new UberX("234ASM",new Account("Amparo","6843518"),"Chevrolet","Spark")
+
+car2.passenger = 4;
+
+car2.printDataCar();
